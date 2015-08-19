a699e4e1f004e1c90c6e924070e0e4e23afc101c
Rafael Lee Munoz <Rafael.Munoz@IGT.com>
2015-08-19 11:14:58 -0400

    Game
    kejhdf

---
+++ ColorPop/Assets/MyScripts/AutoGenerationManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AutoGenerationManager : MonoBehaviour {
+
+	private int firstNumber;
+	private int secondNumber;
+	private int thirdNumber;
+	private int fourthNumber;
+	private int fifthNumber;
+
+	public Text first;
+	public Text second;
+	public Text third;
+	public Text fourth;
+	public Text fifth;
+	
+
+	public void GenerateNumbers(int highestAllowedGeneration)
+	{
+		firstNumber = Random.Range(1, highestAllowedGeneration);
+		secondNumber = Random.Range(1, highestAllowedGeneration);
+		thirdNumber = Random.Range(1, highestAllowedGeneration);
+		fourthNumber = Random.Range(1, highestAllowedGeneration);
+		fifthNumber = Random.Range(1, highestAllowedGeneration);
+
+		first.text = firstNumber.ToString();
+		second.text = secondNumber.ToString();
+		third.text = thirdNumber.ToString();
+		fourth.text = fourthNumber.ToString();
+		fifth.text = fifthNumber.ToString();
+		
+	}
+
+}
