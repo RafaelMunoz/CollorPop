1d8e5d3f85322053fb75d2b1b5bf362d394df2a2
MatthewLaBianca <Matthew.Labianca@IGT.com>
2015-08-27 13:43:57 -0400

    yoooo this is the mouse!!!
    check the lag! #success

---
+++ ColorPop/Assets/MouseScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseScript : MonoBehaviour {
+	
+
+	public Camera mainCamera;
+	// Use this for initialization
+	void Start () {
+		Cursor.visible = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 temp = Input.mousePosition;
+		temp.z = 20.0f; // Set this to be the distance you want the object to be placed in front of the camera.
+		temp.y += 120.0f;
+		temp.x -= 250.0f;
+		this.transform.localPosition = Camera.main.ScreenToWorldPoint(temp);
+		 }
+
+}
