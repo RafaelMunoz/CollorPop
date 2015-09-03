e128b5d17e26260164be78f4a14ec0d694125d07
MatthewLaBianca <Matthew.Labianca@IGT.com>
2015-09-03 12:44:33 -0400

    New Cursor

--- ColorPop/Assets/MyScripts/MouseScript.cs
+++ ColorPop/Assets/MyScripts/MouseScript.cs
@@ -5,18 +5,22 @@ public class MouseScript : MonoBehaviour {
 	
 
 	public Camera mainCamera;
+
+	public bool showCursor = false;
 	// Use this for initialization
 	void Start () {
-		//Cursor.visible = false;
+		Cursor.visible = showCursor;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 temp = Input.mousePosition;
-		temp.z = 20.0f; // Set this to be the distance you want the object to be placed in front of the camera.
-		temp.y += 140.0f;
-		temp.x -= 350.0f;
-		this.transform.position = Camera.main.ScreenToWorldPoint(temp);
+		Vector3 temp = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
+		//temp.z = 20.0f; // Set this to be the distance you want the object to be placed in front of the camera.
+		//temp.y += 140.0f;
+		//temp.x -= 350.0f;
+		//this.transform.position = Camera.main.ScreenToWorldPoint(temp);
+		//transform.position = Camera.main.ScreenToViewportPoint(temp);
+		this.transform.position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
 		 }
 
 }
