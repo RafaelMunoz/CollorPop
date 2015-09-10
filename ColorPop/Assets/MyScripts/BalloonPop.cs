ee5b831fe227282908ce2d98963e2ffa0f377606
RAlonzo <ruben.nunez@igt.com>
2015-09-10 12:43:44 -0400

    Edited Splatter
    Splatter now only randoms of same color.

--- ColorPop/Assets/MyScripts/BalloonPop.cs
+++ ColorPop/Assets/MyScripts/BalloonPop.cs
@@ -5,28 +5,34 @@ using System.Collections.Generic;
 public class BalloonPop : MonoBehaviour {
 
 	private int i;
+	public int colors;
+
 	public GameObject number1;
+	public GameObject splatter1;
+	public GameObject splatter2;
+
+	public AudioClip popSound;
+	private AudioSource source;
 
 	private NumberManager numMan;
 
 	void Start(){
+
 		i = 0;
+		source = GetComponent<AudioSource>();
 		numMan = GameObject.Find ("ScriptHolder").GetComponent<NumberManager> ();
+
 	}
+
 	//public GameObject instantiatedObj;
 	void OnMouseDown()
 	{
-		GameObject splatter1 = GameObject.FindGameObjectWithTag("p" + Random.Range(1, 16));
-		GameObject splatter2 = GameObject.FindGameObjectWithTag("p" + Random.Range(1, 16)); 
-		GameObject splatter3 = GameObject.FindGameObjectWithTag("p" + Random.Range(1, 16)); 
-		GameObject splatter4 = GameObject.FindGameObjectWithTag("p" + Random.Range(1, 16)); 
-
+		source.PlayOneShot(popSound);
+		colors = Random.Range(1, 4);
+		splatter(colors);
 
 		splatter1.GetComponent<Renderer>().enabled = true;
 		splatter2.GetComponent<Renderer>().enabled = true;
-		splatter3.GetComponent<Renderer>().enabled = true;
-		splatter4.GetComponent<Renderer>().enabled = true;
-
 
 		int randomNumber = Random.Range(1,40);
 		GameObject Number = Resources.Load(randomNumber.ToString(), typeof (GameObject)) as GameObject;
@@ -35,9 +41,31 @@ public class BalloonPop : MonoBehaviour {
 			Instantiate (Number, new Vector3 (this.transform.position.x, this.transform.position.y, 40), Quaternion.identity);
 		}
 		SwitchBalloon(false);
-
 	}
-	
+
+	private void splatter(int color)
+	{
+		switch (color)
+		{
+		case 4:
+			splatter1 = GameObject.FindGameObjectWithTag("y" + Random.Range(1, 4));
+			splatter2 = GameObject.FindGameObjectWithTag("y" + Random.Range(1, 4));
+			break;
+		
+		case 3:
+			splatter1 = GameObject.FindGameObjectWithTag("g" + Random.Range(1, 4));
+			splatter2 = GameObject.FindGameObjectWithTag("g" + Random.Range(1, 4));
+			break;
+		case 2:
+			splatter1 = GameObject.FindGameObjectWithTag("r" + Random.Range(1, 4));
+			splatter2 = GameObject.FindGameObjectWithTag("r" + Random.Range(1, 4));
+			break;
+		case 1:
+			splatter1 = GameObject.FindGameObjectWithTag("b" + Random.Range(1, 4));
+			splatter2 = GameObject.FindGameObjectWithTag("b" + Random.Range(1, 4));
+			break;
+		}
+	}
 
 	private void SwitchBalloon(bool on)
 	{
